cbad70c8592a1b14b053a32c577551949309d715
Akın <72783002+akinyolci@users.noreply.github.com>
2023-02-04 22:44:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req,res)=>{
     console.log('Nev connection')
-    res.end('Hello Awesome')
+    res.end('Hello Awesome -now with continous deployment')
 })
 const PORT = process.nextTick.PORT || 8080
 server.listen(PORT, () => console.log('Listening'))
